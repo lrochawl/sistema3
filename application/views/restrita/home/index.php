e42c090c481f5e5ce1a6de4c3a7770a90172284f
Lucas Rocha <wltopos@gmail.com>
2023-01-13 17:32:56 -0300

    teste restrita index

--- application/views/restrita/home/index.php
+++ application/views/restrita/home/index.php
@@ -2,20 +2,7 @@
 <html lang="en">
 
 
-<!-- blank.html  21 Nov 2019 03:54:41 GMT -->
-<head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Otika - Admin Dashboard Template</title>
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="<?php base_url('assets/css/app.min.css'); ?>">
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="<?php base_url('assets/css/style.css'); ?>">
-  <link rel="stylesheet" href="<?php base_url('assets/css/components.css'); ?>">
-  <!-- Custom style CSS -->
-  <link rel="stylesheet" href="<?php base_url('assets/css/custom.css'); ?>">
-  <link rel='shortcut icon' type='image/x-icon' href='<?php base_url("assets/img/favicon.ico"); ?>' />
-</head>
+<?php echo base_url(); ?>
 
 
 
