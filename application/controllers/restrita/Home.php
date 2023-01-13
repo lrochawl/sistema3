7e45d750f0a13b16c1d0b991d690498a0d0e29b4
Lucas Rocha <wltopos@gmail.com>
2023-01-13 19:13:40 -0300

    corrige index restrita

--- application/controllers/restrita/Home.php
+++ application/controllers/restrita/Home.php
@@ -11,7 +11,7 @@ class Home extends CI_Controller{
     }
 
     public function index(){
-        $this->load->view('restrita/layout/Header');
+        $this->load->view('restrita/layout/header');
         $this->load->view('restrita/home/index');
         $this->load->view('restrita/layout/footer');
     }
