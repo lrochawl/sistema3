c448f2ac1b24d2e08f04bb9a9f4e0b8315cd0928
Lucas Rocha <wltopos@gmail.com>
2023-01-14 14:50:16 -0300

    debug

--- application/views/restrita/home/Core.php
+++ application/views/restrita/home/Core.php
@@ -13,6 +13,7 @@
                             <h4><?php echo $titulo ?></h4>
                         </div>
                         <form name="form_core">
+                        <?php print_r($perfil) ?>
                             <div class="card-body">
                                 <div class="form-row">
                                     <div class="form-group col-md-4">
