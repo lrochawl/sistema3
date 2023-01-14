5ba406f85e90e449ec6d2061b0b527ae33d02d43
Lucas Rocha <wltopos@gmail.com>
2023-01-14 08:00:42 -0300

    carregando scripts e styles dinamicos

--- application/views/restrita/layout/Header.php
+++ application/views/restrita/layout/Header.php
@@ -6,12 +6,22 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title><?php echo isset($titulo) ? 'Loja Virtual |'.$titulo: 'Loja Virtual - WL TOPOS' ; ?></title>
+  <title><?php echo isset($titulo) ? 'Loja Virtual | '.$titulo: 'Loja Virtual - WL TOPOS' ; ?></title>
   <!-- General CSS Files -->
   <link rel="stylesheet" href="<?php echo base_url('public/assets/css/app.min.css'); ?>">
   <!-- Template CSS -->
   <link rel="stylesheet" href="<?php echo base_url('public/assets/css/style.css'); ?>">
   <link rel="stylesheet" href="<?php echo base_url('public/assets/css/components.css'); ?>">
+
+<?php if(isset($styles)):?>
+
+  <?php foreach($styles as $style):?>
+    <link rel="stylesheet" href="<?php echo base_url('public/assets/'.$style); ?>">
+  <?php endforeach ?>
+
+<?php endif ?>
+
+
   <!-- Custom style CSS -->
   <link rel="stylesheet" href="<?php echo base_url('public/assets/css/custom.css'); ?>">
   <link rel='shortcut icon' type='image/x-icon' href='<?php echo base_url("public/assets/img/favicon.ico"); ?>' />
