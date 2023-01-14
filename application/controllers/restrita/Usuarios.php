eba4c147b6b4757011e2cf4499090cf614ca675c
Lucas Rocha <wltopos@gmail.com>
2023-01-14 07:43:42 -0300

    Configurando array data

--- application/controllers/restrita/Usuarios.php
+++ application/controllers/restrita/Usuarios.php
@@ -12,7 +12,19 @@ class Usuarios extends CI_Controller {
     public function index(){
 
         $data = array(
-            'usuarios' => $this->ion_auth->users()->result()
+            'titulo' => 'Usuarios cadastrados',
+            'usuarios' => $this->ion_auth->users()->result(),
+            'styles' => array(
+                'bundles/datatables/datatables.min.css',
+                'bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css'
+            ),
+            'scripts' => array(
+                'bundles/datatables/datatables.min.js',
+                'bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js',
+                'bundles/jquery-ui/jquery-ui.min.js',
+                'js/page/datatables.js',
+
+            ),
         );
 
         $this->load->view('restrita/layout/Header', $data);
