dd5a46f8d2ff9e38b3ee89477c7d62fb457f3980
Lucas Rocha <wltopos@gmail.com>
2023-01-14 08:10:37 -0300

    configura data-table.js

--- application/views/restrita/usuarios/Index.php
+++ application/views/restrita/usuarios/Index.php
@@ -16,7 +16,7 @@
                   </div>
                   <div class="card-body">
                     <div class="table-responsive">
-                      <table class="table table-striped" id="table-1">
+                      <table class="table table-striped data-table" >
                         <thead>
                           <tr>
                             <th class="text-center">
