ff88bdb6db3a27fc1ea42af19edb2b157430e2b9
Lucas Rocha <wltopos@gmail.com>
2023-01-17 09:49:17 -0300

    url cadastrar usuario

--- application/views/restrita/usuarios/Index.php
+++ application/views/restrita/usuarios/Index.php
@@ -11,7 +11,7 @@
                 <div class="card">
                   <div class="card-header d-block">
                     <h4><?php echo $titulo ?></h4>
-                    <a class='btn btn-primary float-right' href="">Cadastrar</a>
+                    <a class="btn btn-primary float-right" href="<?php echo base_url('restrita/usuarios/core'); ?>">Cadastrar</a>
                   </div>
                   <div class="card-body">
                  <?php if($message = $this->session->flashdata('sucesso')): ?>
