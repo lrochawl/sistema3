765f3168469d59c229275ebf80db62ef0bc393ba
Lucas Rocha <wltopos@gmail.com>
2023-01-17 08:20:29 -0300

    debug

--- application/controllers/restrita/Usuarios.php
+++ application/controllers/restrita/Usuarios.php
@@ -81,6 +81,7 @@ class Usuarios extends CI_Controller
                     if ($this->ion_auth->update($usuario_id, $data)) {
 
                         $perfil = $this->input->post('perfil');
+                        echo `$perfil $usuario_id`
                         if ($perfil_id) {
                             $this->ion_auth->remove_from_group(NULL, $usuario_id);
                             $this->ion_auth->add_group($perfil, $usuario_id);
