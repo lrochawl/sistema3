0523047a1b10d8390a743fe3f428782964ccc753
Lucas Rocha <wltopos@gmail.com>
2023-01-17 09:37:54 -0300

    corrige botão cadastrar

--- application/views/restrita/home/Core.php
+++ application/views/restrita/home/Core.php
@@ -9,9 +9,8 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card">
-                        <div class="card-header d-block">
+                        <div class="card-header">
                             <h4><?php echo $titulo ?></h4>
-                            <a class='btn btn-primary float-right'>Cadastrar</a>
                         </div>
                         <?php 
                         if (isset($usuario)) {
