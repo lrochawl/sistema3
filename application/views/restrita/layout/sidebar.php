e01305ef1b805af1a65f1a67ed1327e7b857aba9
Lucas Rocha <wltopos@gmail.com>
2023-01-17 09:17:14 -0300

    criando rota index

--- application/views/restrita/layout/sidebar.php
+++ application/views/restrita/layout/sidebar.php
@@ -8,7 +8,7 @@
           <ul class="sidebar-menu">
             <li class="menu-header">Menu</li>
             <li class="dropdown">
-              <a href="<?php echo base_url('index.html') ?>" class="nav-link"><i data-feather="home"></i><span>Inicio</span></a>
+              <a href="<?php echo base_url('restrita') ?>" class="nav-link"><i data-feather="home"></i><span>Inicio</span></a>
             </li>
             <li class="dropdown">
               <a href="#" class="menu-toggle nav-link has-dropdown"><i
