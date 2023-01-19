c6807ced8fa8b6ce10fc3487f1aeed2974445ac8
Lucas Rocha <wltopos@gmail.com>
2023-01-19 13:12:26 -0300

    botao delete

--- application/views/restrita/layout/Footer.php
+++ application/views/restrita/layout/Footer.php
@@ -19,9 +19,22 @@
 
 <?php endif ?>
 <!-- Template JS File -->
-<script src="<?php echo base_url('public/assets/js/scripts.js') ?>"></>
+<script src="<?php echo base_url('public/assets/js/scripts.js') ?>"></script>
 <!-- Custom JS File -->
 <script src="<?php echo base_url('public/assets/js/custom.js') ?>"></script>
+<script>
+  $('.delete').on('click', function(event){
+      event.preventDefault();
+
+    var choice = confirm($(this).attr('data-confirm'));
+
+    if(choice){
+      window.location.href = $(this).attr('href');
+    }
+
+  });
+
+  </script>
 </body>
 
 
