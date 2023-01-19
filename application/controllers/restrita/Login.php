6a9163b98fac87d1ecb0b8791dbc32aa16b84acb
Lucas Rocha <wltopos@gmail.com>
2023-01-19 16:06:40 -0300

    pagina de login restrito

---
+++ application/controllers/restrita/Login.php
@@ -0,0 +1,20 @@
+<?php 
+defined('BASEPATH') OR exit('Ação não permitida');
+
+class Login extends CI_Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function index(){
+        $data = array(
+            'titulo' => 'Login area restrita',
+        );
+
+        $this->load->view('retrita/layout/Header');
+        $this->load->view('restrita/login/Index');
+        $this->load->view('restrita/layout/Settings');
+        $this->load->view('restrita/layout/Footer');
+    }
+
+}
